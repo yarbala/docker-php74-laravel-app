ee8919845e9ddeb2017aeadce52522205663bb0d
yarbala <yarbala@gmail.com>
2021-02-16 15:41:50 +0100

    php fpm new settings

--- push.sh
+++ push.sh
@@ -3,8 +3,8 @@
 # Exit on any error
 set -e
 
-docker build --squash -t yarbala/php74-laravel-app-front:latest -t yarbala/php74-laravel-app-front:v1.0.10 -f front.Dockerfile .
-docker build --squash -t yarbala/php74-laravel-app-cli:latest -t yarbala/php74-laravel-app-cli:v1.0.10 -f cli.Dockerfile .
+docker build --squash -t yarbala/php74-laravel-app-front:latest -t yarbala/php74-laravel-app-front:v1.1.1 -f front.Dockerfile .
+docker build --squash -t yarbala/php74-laravel-app-cli:latest -t yarbala/php74-laravel-app-cli:v1.1.1 -f cli.Dockerfile .
 
-docker push yarbala/php74-laravel-app-front:v1.0.10
-docker push yarbala/php74-laravel-app-cli:v1.0.10
+docker push yarbala/php74-laravel-app-front:v1.1.1
+docker push yarbala/php74-laravel-app-cli:v1.1.1
